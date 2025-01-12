33300107c493f308504301a4c6c0d3371ebc4b07
Aidan Campbell <aidanv.campbell@mail.utoronto.ca>
2025-01-12 10:37:41 -0500

    Reviewed requested changes. Removed incorrect removal command from script.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -37,6 +37,7 @@ cp ./data/raw/event*.log ./data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
+#There was an attempt to add "rf -rf ./data" here, but this isn't a command and removing ./data would nullify the script.
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch ./data/inventory.txt
 ls -R ./data/processed > ./data/inventory.txt
